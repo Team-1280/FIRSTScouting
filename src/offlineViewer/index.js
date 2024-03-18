3920b188caaef24799d40c2d9bb15708047feace
Team 1280 Programming Laptop <59985235+Team1280Programming@users.noreply.github.com>
2024-03-18 16:13:04 -0700

    fix: remove message domain checking

--- src/offlineViewer/index.js
+++ src/offlineViewer/index.js
@@ -1,11 +1,5 @@
 window.onload = function () {
     function receiveMessage(e) {
-        if (
-            e.origin !== `http://${window.location.host.split(':')[0]}:8000` ||
-            e.origin !== `https://1280scouting.vercel.app/`
-        )
-            return
-
         let data = JSON.parse(e.data)
         let existingData = localStorage.getItem('fieldData')
 
