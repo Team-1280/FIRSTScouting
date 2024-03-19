be34a83b887141a7ded44367a1280b64bf3edc7c
QLabs <55121845+Quantalabs@users.noreply.github.com>
2024-03-18 22:19:26 -0700

    perf: remove unnecessary code blocks

--- pass/resources/js/googleSheets.js
+++ pass/resources/js/googleSheets.js
@@ -1,7 +1,4 @@
 function setUpGoogleSheets() {
-    const scriptURL = `http://${
-        window.location.hostname.split(":")[0]
-    }:3000/data`
     const form = document.querySelector("#scoutingForm")
     const btn = document.querySelector("#submit")
 
@@ -14,18 +11,9 @@ function setUpGoogleSheets() {
 
         const parsedData = {}
         pairs.forEach((pair) => {
-          const [key, value] = pair.split("=")
-          parsedData[key] = value
-      })
-
-        // URL encode the parsed data
-        // const fd = new URLSearchParams(
-        //     parsedData
-        // ).toString()
-        
-        // fetch(scriptURL + "?" + fd, {
-        //     mode: "no-cors"
-        // })
+            const [key, value] = pair.split("=")
+            parsedData[key] = value
+        })
 
         btn.disabled = false
         btn.innerHTML = "Send to Database"
